fde60b01e1e9567ea769991ff9010ec0d344c8ae
yukta <iit2019052@iiita.ac.in>
2020-05-02 22:40:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/test3/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.example.test3.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Prepare, Don't Panic");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
